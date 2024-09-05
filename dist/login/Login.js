3a34cc6c2b72bf717ce20f0440a6724669ba7976
aish13090 <saishvarya24ece@srishakthi.ac.in>
2024-09-05 17:35:28 +0530

    h

---
+++ dist/login/Login.js
@@ -0,0 +1,15 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+var _react = _interopRequireDefault(require("react"));
+require("./Login.css");
+function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
+var Login = function Login() {
+  return /*#__PURE__*/_react["default"].createElement("div", {
+    className: "login"
+  }, "Login");
+};
+var _default = exports["default"] = Login;
